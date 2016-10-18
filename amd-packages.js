bc0cb6eb76ff2796de2d44cd1a055e427593ade6
priemd <david@thepriems.com>
2016-10-18 14:05:16 -0400

    Create amd-packages.js

---
+++ amd-packages.js
@@ -0,0 +1,5 @@
+require(packages: [
+    { name: "dgrid", location: "https://cdn.rawgit.com/SitePen/dgrid/v0.3.1"},
+    { name: "gridx", location: "https://rawgit.com/oria/gridx/tree/v1.3.9"},
+    ]
+});
